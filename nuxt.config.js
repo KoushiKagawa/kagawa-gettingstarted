cdf07cfa4ac41f092b18412ec625aa885d4590f7
Koushi Kagawa <kkagawa@diverta.co.jp>
2022-02-09 11:47:52 +0900

    fix: add secret

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,9 +36,14 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+  
+  privateRuntimeConfig: {
+    baseURL: process.env.BASE_URL
+  },
 }
